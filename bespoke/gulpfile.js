6d179d8d0c221996ae1462a4066b1ac5ab50dd20
jrhicks@cteh.com <ghfrog78>
2014-08-04 22:16:27 -0500

    bespoke

---
+++ bespoke/gulpfile.js
@@ -0,0 +1,74 @@
+var pkg = require('./package.json'),
+  gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  plumber = require('gulp-plumber'),
+  //rimraf = require('gulp-rimraf'),
+  rename = require('gulp-rename'),
+  connect = require('gulp-connect'),
+  browserify = require('gulp-browserify'),
+  uglify = require('gulp-uglify'),
+  jade = require('gulp-jade'),
+  stylus = require('gulp-stylus'),
+  autoprefixer = require('gulp-autoprefixer'),
+  csso = require('gulp-csso'),
+  through = require('through'),
+  opn = require('opn'),
+  path = require('path'),
+  es = require('event-stream');
+
+
+var isDist = true; // process.argv.indexOf('serve') === -1;
+
+function js() {
+    var dest = path.join(process.cwd(), 'build', 'bespoke');
+
+    return gulp.src(path.join(__dirname, 'src/scripts/main.js'))
+        .pipe(isDist ? through() : plumber())
+        .pipe(browserify({ transform: ['debowerify'], debug: !isDist }))
+        .pipe(isDist ? uglify() : through())
+        .pipe(rename('build.js'))
+        .pipe(gulp.dest(dest))
+}
+gulp.task('js', js);
+
+function css() {
+    var dest = path.join(process.cwd(), 'build', 'bespoke');
+
+    return gulp.src(path.join(__dirname, 'src/styles/main.styl'))
+        .pipe(isDist ? through() : plumber())
+        .pipe(stylus({
+            // Allow CSS to be imported from node_modules and bower_components
+            'include css': true,
+            'paths': [path.join(__dirname, 'node_modules'), path.join(__dirname,'bower_components')]
+        }))
+        .pipe(autoprefixer('last 2 versions', { map: false }))
+        .pipe(isDist ? csso() : through())
+        .pipe(rename('build.css'))
+        .pipe(gulp.dest(dest))
+}
+
+gulp.task('css', css);
+
+function images() {
+    var dest = path.join(process.cwd(), 'build', 'bespoke');
+
+    var d = path.join(dest, 'images');
+    return gulp.src(path.join(__dirname, 'src/images/**/*'))
+        .pipe(gulp.dest(d))
+}
+
+gulp.task('images', images);
+
+function build(cb) {
+    var dest = path.join(process.cwd(), 'build', 'bespoke');
+
+    es.merge(js(), css(), images())
+        .on('end', function() {
+        cb()
+        });
+}
+
+gulp.task('build', ['js', 'css', 'images']);
+gulp.task('default', ['build']);
+
+module.exports = build;
--- bespoke/index.js
+++ bespoke/index.js
@@ -1,14 +1,9 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var inject = require("gulp-inject");
-var es = require('event-stream');
-var wiredep = require('wiredep');
-
 var marked = require('marked');
 var highlight = require('highlight.js');
 var ejs = require('ejs');
 var xregexp = require('xregexp');
-
 var path = require('path');
 var fs = require("fs");
 var renderer = new marked.Renderer();
@@ -35,14 +30,6 @@ function customizeMarked() {
 
 }
 
-function leadify_h1s(html) {
-    return html;
-    var re = xregexp.XRegExp;
-    var lRegex = re('(?<body><h1.*?)(?<pstart>(<p)(<prest>.*?</p>)(?<tail>(<!-- h[1-9] -->|<!-- end -->))','sgi')
-    html = re.replace(html,lRegex,"${body}${pstart} class='lead' ${prest}${tail}");
-    return html;
-}
-
 function endify_headings(html) {
     var re = xregexp.XRegExp;
     var heading = re('<(?<heading>h[1-9])','sgi');
@@ -52,14 +39,12 @@ function endify_headings(html) {
 }
 
 
-function rowify_h3s(html) {
+function sectionify_headings(html) {
     var re = xregexp.XRegExp;
 
-    var rowRegex = re('(?<body><h3.*?)(?<tail>(<!-- h[12] -->|<!-- end -->))','sgi')
-    var colRegex = re('(?<body><h3.*?)(?<tail>(<!-- h[123] -->|<!-- end -->))','sgi')
+    var rowRegex = re('(?<body><h[2345].*?)(?<tail>(<!-- h[12345] -->|<!-- end -->))','sgi')
 
-    html = re.replace(html,rowRegex,"<div class='row'>${body}</div>${tail}");
-    html = re.replace(html,colRegex,"<div class='col-sm-4'>${body}</div>${tail}");
+    html = re.replace(html,rowRegex,"<section>${body}</section>${tail}");
     return html;
 }
 
@@ -69,10 +54,7 @@ function render(file) {
 
     var h = marked(file.body);;
     var h2 = endify_headings(h);
-    var h3 = rowify_h3s(h2);
-    //var h = leadify_h1s(h);
-
-    //gutil.log(JSON.stringify(marked.lexer(file.body)))
+    var h3 = sectionify_headings(h2);
 
     return ejs.render(template, {
         params: file.params,
